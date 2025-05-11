c017d3aad5c94974c7bd575c4d2269a6ad72e70c
samuelalonso96 <salonso.cpp@gmail.com>
2025-05-11 09:59:34 +0100

    Added Core Safety

--- Source/Core/Safety.hpp
+++ Source/Core/Safety.hpp
@@ -1,12 +1,12 @@
 #if lacia_major >= 0 and lacia_middle >= 0 and lacia_minor >= 1
 #ifndef lacia_guard_core_safety
 #define lacia_guard_core_safety
-#define $reliable(...) (::Lucy::Detail::Safe<> __VA_OPT__(,) __VA_ARGS__)
-#define $unreliable(...) (::Lucy::Detail::Safe<false> __VA_OPT__(,) __VA_ARGS__)
+#define $reliable(...) (::Lacia::Detail::Safe<> __VA_OPT__(,) __VA_ARGS__)
+#define $unreliable(...) (::Lacia::Detail::Safe<false> __VA_OPT__(,) __VA_ARGS__)
 #define $(...) (do_not_use_safe_object_private_and_secret __VA_OPT__(,) __VA_ARGS__)
-#define $unsafe if constexpr([[maybe_unused]] constexpr ::Lucy::Detail::Safe<false> \
+#define $unsafe if constexpr([[maybe_unused]] constexpr ::Lacia::Detail::Safe<false> \
 do_not_use_safe_object_private_and_secret; true)
-#define $safe if constexpr([[maybe_unused]] constexpr ::Lucy::Detail::Safe<> \
+#define $safe if constexpr([[maybe_unused]] constexpr ::Lacia::Detail::Safe<> \
 do_not_use_safe_object_private_and_secret; true)
 
 namespace Lacia::Detail
